fc0f1eb65848583c667bef50b7b1df982b853b36
Bernardino Villagra <bernardinovillagra@hotmail.com>
2022-02-06 18:26:12 -0300

    Modelo y migracion SaleDetails

---
+++ database/migrations/2022_02_06_211932_create_sale_details_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_details', function (Blueprint $table) {
+            $table->id();
+
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
+
+            $table->unsignedBigInteger('sale_id');
+            $table->unsignedBigInteger('product_id');
+
+            $table->foreign('sale_id')
+                ->references('id')
+                ->on('sales');
+
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_details');
+    }
+}
